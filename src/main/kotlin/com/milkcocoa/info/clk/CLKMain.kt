c724df353b4d31cfe805fbd328ce372f75a90064
keita <developer@milkcocoa.info>
2023-12-26 23:57:09 +0900

    edit CLKMain(sample)

--- src/main/kotlin/com/milkcocoa/info/clk/CLKMain.kt
+++ src/main/kotlin/com/milkcocoa/info/clk/CLKMain.kt
@@ -2,6 +2,7 @@ package com.milkcocoa.info.clk
 
 import com.milkcocoa.info.clk.core.formatter.builtin.structure.DetailStructureFormatter
 import com.milkcocoa.info.clk.core.formatter.builtin.structure.SimpleStructureFormatter
+import com.milkcocoa.info.clk.core.formatter.builtin.text.DetailTextFormatter
 import com.milkcocoa.info.clk.core.logger.LogLevel
 import com.milkcocoa.info.clk.core.logger.LoggerFactory
 import com.milkcocoa.info.clk.core.formatter.details.LogStructure
@@ -14,16 +15,19 @@ import java.nio.file.Path
 class CLKMain
 
 @Serializable
-class A(val b: String, val ccc: String): LogStructure
+class LogDetail(val scope: String, val message: String): LogStructure
 
 @Serializable
-class AAA(val ddd: String, val a: A): LogStructure
+class Log(val name: String, val logDetail: LogDetail): LogStructure
 
 fun main() {
 val fileProvider: FileProvider
 val logger = LoggerFactory()
     .addProvider(ConsoleProvider{
         formatter = DetailStructureFormatter
+    })
+    .addProvider(ConsoleProvider{
+        formatter = DetailTextFormatter
     })
     .addProvider(FileProvider(Path.of("./test.log")){
         logLevel = LogLevel.TRACE
@@ -36,17 +40,39 @@ val logger = LoggerFactory()
     })
     .getLogger()
 
-    logger.info(AAA("afaffaefe", A("aaa", "afsdf")), AAA.serializer())
+    logger.info(
+        Log(
+            name = "illegal state",
+            LogDetail(
+                "args",
+                "argument must be greater than zero"
+            )
+        ),
+        Log.serializer()
+    )
 
-//    logger.trace("TRACE LEVEL LOG")
-//    logger.debug("DEBUG LEVEL LOG")
-//    logger.info("INFO LEVEL LOG")
-//    logger.warn("WARN LEVEL LOG")
-//    logger.error("ERROR LEVEL LOG")
 
-//    logger.atInfo {
-//        print("in this block")
-//        print("all of logs are printed out with INFO level")
-//    }
+
+    logger.trace("TRACE LEVEL LOG")
+    logger.debug("DEBUG LEVEL LOG")
+    logger.info("INFO LEVEL LOG")
+    logger.warn("WARN LEVEL LOG")
+    logger.error("ERROR LEVEL LOG")
+
+    logger.atInfo {
+        print("in this block")
+        print("all of logs are printed out with INFO level")
+
+        print(
+            Log(
+                name = "illegal state",
+                LogDetail(
+                    "args",
+                    "argument must be greater than zero"
+                )
+            ),
+            Log.serializer()
+        )
+    }
     fileProvider.flush()
 }
