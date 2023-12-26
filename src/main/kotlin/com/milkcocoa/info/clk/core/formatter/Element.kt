068272400adb54c15aae030bcc08386461916e4c
keita <developer@milkcocoa.info>
2023-12-26 23:17:03 +0900

    add comment and attrs to LogElement

--- src/main/kotlin/com/milkcocoa/info/clk/core/formatter/Element.kt
+++ src/main/kotlin/com/milkcocoa/info/clk/core/formatter/Element.kt
@@ -1,16 +1,62 @@
 package com.milkcocoa.info.clk.core.formatter
 
 enum class Element(private val typeString: String) {
+    /**
+     * year, ignored when StructuredFormat
+     */
     YEAR("%Y"),
+
+    /**
+     * month, ignored when StructuredFormat
+     */
     MONTH("%M"),
+
+    /**
+     * day, ignored when StructuredFormat
+     */
     DAY("%D"),
+
+    /**
+     * hour, ignored when StructuredFormat
+     */
     HOUR("%h"),
+
+    /**
+     * minute, ignored when StructuredFormat
+     */
     MINUTE("%m"),
+
+    /**
+     * second, ignored when StructuedFormat
+     */
     SECOND("%s"),
 
+    /**
+     * date(yyyy-MM-dd)
+     */
+    DATE("%d"),
+
+    /**
+     * time(HH:mm:ss)
+     */
+    TIME("%T"),
+
+    /**
+     * log level
+     */
     LEVEL("%L"),
+
+    /**
+     * log message
+     */
     MESSAGE("%l"),
-    THREAD("%t");
+
+    /**
+     * logging thread
+     */
+    THREAD("%t"),
+
+    ATTR("%a");
 
     override fun toString(): String {
         return typeString
