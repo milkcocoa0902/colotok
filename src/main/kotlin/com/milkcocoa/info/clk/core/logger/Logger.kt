4f8c65b16dad254587078edfd258e39c31835cea
keita <developer@milkcocoa.info>
2023-12-26 23:17:20 +0900

    StructuredLog output

--- src/main/kotlin/com/milkcocoa/info/clk/core/logger/Logger.kt
+++ src/main/kotlin/com/milkcocoa/info/clk/core/logger/Logger.kt
@@ -1,5 +1,13 @@
 package com.milkcocoa.info.clk.core.logger
 
+import com.milkcocoa.info.clk.core.formatter.details.LogStructure
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
 class Logger(val name: String, val config: Config) {
     constructor(name: String, config: Config.() -> Unit) : this(name = name, Config().apply(config))
 
@@ -33,6 +41,42 @@ class Logger(val name: String, val config: Config) {
             it.write(name, str, LogLevel.ERROR)
         }
     }
+
+
+
+
+
+
+    fun<T: LogStructure> trace(msg: T, serializer: KSerializer<T>) {
+        providers.forEach {
+            it.write(name, msg, serializer, LogLevel.TRACE)
+        }
+    }
+
+    fun<T: LogStructure> debug(msg: T, serializer: KSerializer<T>) {
+        providers.forEach {
+            it.write(name, msg, serializer, LogLevel.DEBUG)
+        }
+    }
+
+    fun<T: LogStructure> info(msg: T, serializer: KSerializer<T>) {
+        providers.forEach {
+            it.write(name, msg, serializer, LogLevel.INFO)
+        }
+    }
+
+    fun<T: LogStructure> warn(msg: T, serializer: KSerializer<T>) {
+        providers.forEach {
+            it.write(name, msg, serializer, LogLevel.WARN)
+        }
+    }
+
+    fun<T: LogStructure> error(msg: T, serializer: KSerializer<T>) {
+        providers.forEach {
+            it.write(name, msg, serializer, LogLevel.ERROR)
+        }
+    }
+
 
     fun atTrace(block: LevelScopedLogger.() -> Unit){
         block(LevelScopedLogger(name, config, LogLevel.TRACE))
