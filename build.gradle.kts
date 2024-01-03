da6c19b755f270813b9d1595dbf8fab91933f526
keita <developer@milkcocoa.info>
2024-01-03 21:30:25 +0900

    :wrench: add a mockk for test with mocking

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-properties:1.6.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.mockk:mockk:1.13.7")
 }
 
 tasks.test {
