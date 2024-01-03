46455c1e9a3a2f50f216445e97f668c9fc0837d7
keita <developer@milkcocoa.info>
2024-01-03 16:43:15 +0900

    :bookmark: release new version, 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.milkcocoa0902"
-version = "0.1.9"
+version = "0.2.0"
 java.sourceCompatibility = JavaVersion.VERSION_11
 tasks.withType<KotlinCompile> {
     kotlinOptions {
