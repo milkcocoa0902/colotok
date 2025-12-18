ee2f40236bb4c04b02d05d2c94804e07ada5d3c0
keita <developer@milkcocoa.info>
2025-12-18 23:08:17 +0900

    :memo: fix sample

--- sample/src/main/kotlin/CLKMain.kt
+++ sample/src/main/kotlin/CLKMain.kt
@@ -1,10 +1,8 @@
 package com.milkcocoa.info.colotok
 
 import com.milkcocoa.info.colotok.core.formatter.details.LogStructure
-import com.milkcocoa.info.colotok.core.logger.ColotokLoggerFactory
+import com.milkcocoa.info.colotok.core.logger.ColotokLoggerContext
 import com.milkcocoa.info.colotok.core.provider.builtin.console.ConsoleProvider
-import com.milkcocoa.info.colotok.core.provider.cloudwatch.CloudwatchCredential
-import com.milkcocoa.info.colotok.core.provider.cloudwatch.CloudwatchProvider
 import kotlinx.serialization.Serializable
 
 class CLKMain
@@ -23,15 +21,7 @@ class Credential(
 ) : LogStructure
 
 fun main() {
-    val logger = ColotokLoggerFactory()
-        .addProvider(CloudwatchProvider{
-            this.credential = CloudwatchCredential.Default(
-                region = "us-east-1",
-            )
-            this.logBufferSize = 0
-            this.logGroup = "colotok"
-            this.logStream = "2025-06-22"
-        })
+    val logger = ColotokLoggerContext()
         .addProvider(ConsoleProvider())
         .getLogger()
 
